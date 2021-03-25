1e8930048c7c0b4f6ed44f3d165717753d0ca335
scottedwardpratt <prattsc@msu.edu>
2021-03-25 10:20:38 -0400

    added files

---
+++ software/include/acceptancebal.h
@@ -0,0 +1,14 @@
+#ifndef __ACCEPTANCE_BAL_H__
+#define __ACCEPTANCE_BAL_H__
+using namespace std;
+class CHBTPart;
+
+class CAcceptanceBal{
+public:
+	CAcceptanceBal(CparameterMap *parmap);
+	string ACCEPTANCE;
+	bool acceptance(CHBTPart *part1,CHBTPart *part2,double &efficiency);
+	double etamax,ptmin_pi,ptmin_K,ptmin_p;
+};
+
+#endif
---
+++ software/src/acceptancebal.cc
@@ -0,0 +1,54 @@
+#include "balhbt.h"
+using namespace std;
+
+CAcceptanceBal::CAcceptanceBal(CparameterMap *parmap){
+	ACCEPTANCE=parmap->getS("BF_ACCEPTANCE","STAR");
+	if(ACCEPTANCE=="STAR"){
+		etamax=0.9;
+		ptmin_pi=200.0;
+		ptmin_K=200.0;
+		ptmin_p=400.0;
+	}
+	else if(ACCEPTANCE=="ALICE"){
+		etamax=0.8;
+		ptmin_pi=200.0;
+		ptmin_K=200.0;
+		ptmin_p=200.0;
+	}
+}
+
+bool CAcceptanceBal::acceptance(CHBTPart *part1,CHBTPart *part2,double &efficiency){
+	double pt1,pt2,eta1,eta2;
+	efficiency=0.0;
+	if(abs(part1->resinfo->code)!=211 && abs(part1->resinfo->code)!=321 && abs(part1->resinfo->code)!=2212)
+		return false;
+	if(abs(part2->resinfo->code)!=211 && abs(part2->resinfo->code)!=321 && abs(part2->resinfo->code)!=2212)
+		return false;
+	eta1=atanh(part1->p[3]/part1->p[0]);
+	eta2=atanh(part2->p[3]/part2->p[0]);
+	if(fabs(eta1-eta2)>2.0*etamax){
+		return false;
+	}
+	
+	pt1=sqrt(part1->p[1]*part1->p[1]+part1->p[2]*part1->p[2]);
+	if(abs(part1->resinfo->code)==211 && pt1<ptmin_pi)
+		return false;
+	if(abs(part1->resinfo->code)==321 && pt1<ptmin_K)
+		return false;
+	if(abs(part1->resinfo->code)==2212 && pt1<ptmin_p)
+		return false;
+		
+	pt2=sqrt(part2->p[1]*part2->p[1]+part2->p[2]*part2->p[2]);
+	if(abs(part2->resinfo->code)==211 && pt2<ptmin_pi)
+		return false;
+	if(abs(part2->resinfo->code)==321 && pt2<ptmin_K)
+		return false;
+	if(abs(part2->resinfo->code)==2212 && pt2<ptmin_p)
+		return false;
+	
+	efficiency=1.0;
+	if(ACCEPTANCE=="STAR")
+		efficiency=1.0-0.5*fabs(eta1-eta2)/etamax;
+
+	return true;	
+}
