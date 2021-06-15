d477a2d4b0f19ef25ad6549b5f542a26e9b3aff4
scottedwardpratt <prattsc@msu.edu>
2021-06-15 14:37:44 -0400

    new file

--- software/src/stableinfo.cc
+++ software/src/stableinfo.cc
@@ -16,7 +16,7 @@ void CBalHBT::GetStableInfo(CResList *reslist,double taumax,vector<CStableInfo *
 				stableinfo=new CStableInfo(resinfo);
 				stablevec.push_back(stableinfo);
 				//if(resinfo->decay)
-				printf("PID=%d, tau=%g, %s\n",resinfo->code,HBARC/resinfo->width,resinfo->name.c_str());
+				//printf("PID=%d, tau=%g, %s\n",resinfo->code,HBARC/resinfo->width,resinfo->name.c_str());
 			}
 			id+=1;
 		}
@@ -46,10 +46,10 @@ void CBalHBT::GetStableInfo(CResList *reslist,double taumax,vector<CStableInfo *
 		for(id2=0;id2<NID;id2++){
 			pid2=stablevec[id2]->resinfo->code;
 			bfnorm[id1][id2]=reslist->CalcBalanceNorm(pid1,pid2,taumax);
-			printf("bfnorm[%d][%d]=%g, bfnorm[%d][%d]*dens=%g =? %g\n",id1,id2,bfnorm[id1][id2],id1,id2,
-			bfnorm[id1][id2]*stablevec[id2]->density,bfnorm[id2][id1]*stablevec[id1]->density);
+			//printf("bfnorm[%d][%d]=%g, bfnorm[%d][%d]*dens=%g =? %g\n",id1,id2,bfnorm[id1][id2],id1,id2,
+			//bfnorm[id1][id2]*stablevec[id2]->density,bfnorm[id2][id1]*stablevec[id1]->density);
 		}
-		printf("--------------------------------------\n");
+		//printf("--------------------------------------\n");
 	}
 	
 	double netcharge,netbaryon,netstrange;
@@ -63,7 +63,7 @@ void CBalHBT::GetStableInfo(CResList *reslist,double taumax,vector<CStableInfo *
 			bfnorm[id1][id2]=bfnorm[id1][id2]/stablevec[id1]->density;
 		}
 		CStableInfo::denstot+=2.0*stablevec[id2]->density;
-		printf("%5d: netQ=%8.5f, netB=%8.5f, netS=%8.5f\n",stablevec[id2]->resinfo->code,netcharge,netbaryon,netstrange);
+		//printf("%5d: netQ=%8.5f, netB=%8.5f, netS=%8.5f\n",stablevec[id2]->resinfo->code,netcharge,netbaryon,netstrange);
 	}
 
 }
