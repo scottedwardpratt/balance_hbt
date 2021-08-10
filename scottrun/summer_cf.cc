19f63289bbd53e0e9048f4e2aa24d7ef07b5fd04
scottedwardpratt <prattsc@msu.edu>
2021-08-10 13:49:45 -0400

    minor

--- scottrun/summer_cf.cc
+++ scottrun/summer_cf.cc
@@ -11,10 +11,10 @@ const double HBARC=197.3269602;
 using namespace std;
 
 int main(int argc,char *argv[]){
-	const int NRUNS=12,NQ=100;
+	const int NRUNS=24,NQ=100;
 	int iq,irun;
 	FILE *fptr;
-	char filename[120];
+	char filename[120],dummy[120];
 	double cout,cside,clong,denomout,denomside,denomlong;
 	vector<double> q;
 	vector<double> cf_out,cf_side,cf_long;
@@ -34,6 +34,7 @@ int main(int argc,char *argv[]){
 		sprintf(filename,"results/cf%d_outsidelong.dat",irun);
 		printf("will read from %s\n",filename);
 		fptr=fopen(filename,"r");
+		fgets(dummy,120,fptr);
 		for(iq=0;iq<NQ;iq++){
 			fscanf(fptr,"%lf",&q[iq]);
 			fscanf(fptr,"%lf %lf %lf %lf %lf %lf",
@@ -57,17 +58,17 @@ int main(int argc,char *argv[]){
 	sprintf(filename,"results/cf_outsidelong.dat");
 	fptr=fopen(filename,"w");
 	for(iq=0;iq<NQ;iq++){
-		cf_out[iq]=cf_out[iq]/(NRUNS*denom_out[iq]);
+		cf_out[iq]=cf_out[iq]/(denom_out[iq]);
 		if(cf_out[iq]!=cf_out[iq]){
 			cf_out[iq]=0.0;
 			printf("denom_out[%d]=%g",iq,denom_out[iq]);
 		}
-		cf_side[iq]=cf_side[iq]/(NRUNS*denom_side[iq]);
+		cf_side[iq]=cf_side[iq]/(denom_side[iq]);
 		if(cf_side[iq]!=cf_side[iq]){
 			cf_side[iq]=0.0;
 			printf("denom_side[%d]=%g",iq,denom_side[iq]);
 		}
-		cf_long[iq]=cf_long[iq]/(NRUNS*denom_long[iq]);
+		cf_long[iq]=cf_long[iq]/(denom_long[iq]);
 		if(cf_long[iq]!=cf_long[iq]){
 			cf_long[iq]=0.0;
 			printf("denom_long[%d]=%g",iq,denom_long[iq]);
