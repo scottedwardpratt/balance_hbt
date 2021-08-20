4c7df3ca6f17c1bdd2d30c2bec2d97df737f3d7b
Karina Martirosova <martiros@msu.edu>
2021-08-20 14:11:05 -0400

    update

--- karinarun/bwspectra.cc
+++ karinarun/bwspectra.cc
@@ -9,6 +9,7 @@ void WriteSpectra(vector<double> &spectra,string filename){
 	unsigned int ipt,Nspectra=spectra.size();
 	double pt;
 	FILE *fptr;
+	printf("What's happening?\n");
 	fptr=fopen(filename.c_str(),"w");
 	for(ipt=0;ipt<Nspectra;ipt++){
 		if(ipt<15){
@@ -25,7 +26,7 @@ void WriteSpectra(vector<double> &spectra,string filename){
 int GetIPt_PHENIX(double pt){
 	int ipt=-1;
 	if(pt>500 && pt<20000){
-		ipt=floorl(pt/100)-6;
+		ipt=floorl(pt/100)-5;
 	}
 	if(pt>=2.0 && pt<3.0){
 		ipt=floorl(pt/200)-10+5;
@@ -52,52 +53,32 @@ double GetPTDPTDY_PHENIX(int ipt){
 	return ptdptdy;
 }
 
-double  CalcChiSquared_p(vector<double> &spectra_p, vector<double> &spectra_pi, vector<double> &spectra_K, vector<double> &error_p, vector<double> &error_pi, vector<double> &error_K){
+double  CalcChiSquared_p(vector<double> &spectra_p,vector<double> &error_p){
 	FILE *fptr=fopen("phenix_data/phenix_proton.txt","r");
 	//char dummy[120];
-	double ptbar,ptlow,pthigh,spectra_phenix,stathigh,statlow,syshigh,syslow,sigma2,chi2=0.0,chipi=0.0,chiK=0.0;
+	double ptbar,ptlow,pthigh,spectra_phenix,stathigh,statlow,syshigh,syslow,sigma2,chi2=0.0;
 	int ipt;
-	for(ipt=1;ipt<15;ipt++){
+	for(ipt=0;ipt<20;ipt++){
 		fscanf(fptr,"%lf %lf %lf %lf %lf %lf %lf %lf",&ptbar,&ptlow,&pthigh,&spectra_phenix,&stathigh,&statlow,&syshigh,&syslow);
 		sigma2=0.25*(stathigh-statlow)*(stathigh-statlow)+0.25*(syshigh-syslow)*(syshigh-syslow);
 		sigma2+=error_p[ipt]*error_p[ipt];
 		//printf("pt=%g, phenix_spectra=%g, bw_spectra=%g, sigam2=%g\n",ptbar,spectra_phenix,spectra_p[ipt],sigma2);
 		chi2+=pow(spectra_p[ipt]-spectra_phenix,2)/sigma2;
 	}
-	fclose(fptr);
-	FILE *fptr1=fopen("phenix_data/phenix_pion.txt","r");
-	for(ipt=0;ipt<8;ipt++){
-		fscanf(fptr1,"%lf %lf %lf %lf %lf %lf %lf %lf",&ptbar,&ptlow,&pthigh,&spectra_phenix,&stathigh,&statlow,&syshigh,&syslow);
-		sigma2=0.25*(stathigh-statlow)*(stathigh-statlow)+0.25*(syshigh-syslow)*(syshigh-syslow);
-		sigma2+=error_pi[ipt]*error_pi[ipt];
-		//printf("pt=%g, phenix_spectra=%g, bw_spectra=%g, sigam2=%g\n",ptbar,spectra_phenix,spectra_p[ipt],sigma2);
-		chipi+=pow(spectra_pi[ipt]-spectra_phenix,2)/sigma2;
-	}
-	fclose(fptr1);
-	FILE *fptr2=fopen("phenix_data/phenix_kaon.txt","r");	
-	for(ipt=0;ipt<12;ipt++){
-		fscanf(fptr2,"%lf %lf %lf %lf %lf %lf %lf %lf",&ptbar,&ptlow,&pthigh,&spectra_phenix,&stathigh,&statlow,&syshigh,&syslow);
-		sigma2=0.25*(stathigh-statlow)*(stathigh-statlow)+0.25*(syshigh-syslow)*(syshigh-syslow);
-		sigma2+=error_K[ipt]*error_K[ipt];
-		//printf("pt=%g, phenix_spectra=%g, bw_spectra=%g, sigam2=%g\n",ptbar,spectra_phenix,spectra_p[ipt],sigma2);
-		chiK+=pow(spectra_K[ipt]-spectra_phenix,2)/sigma2;
-	}	
-	return chi2+chipi+chiK;
+	return chi2;
 }
 
 int main(int argc,char *argv[]){
 	int run_number=0;
-	double A1, A2, A3, TF, UPERP;
-	if (argc != 6) {
+	double A,TF,UPERP;
+	if (argc != 4) {
 		printf("Usage: balance_hbt A TF UPERP\n");
 		exit(-1);
 	}
 	else{
-		A1=atof(argv[1]);
-		A2=atof(argv[2]);
-		A3=atof(argv[3]);
-		TF=atof(argv[4]);
-		UPERP=atof(argv[5]);
+		A=atof(argv[1]);
+		TF=atof(argv[2]);
+		UPERP=atof(argv[3]);
 	}
 	CparameterMap parmap;
 	parmap.ReadParsFromFile("parameters/respars.txt");
@@ -111,27 +92,19 @@ int main(int argc,char *argv[]){
 	double ptbar_pi=0.0,ptbar_K=0.0,ptbar_p=0.0;
 	vector<vector<double>> bfnorm;
 	vector<CStableInfo *> stablevec;
-	unsigned int id,id1,i,iprod,ipt,Nspectrap=15, Nspectrapi=8, NspectraK=12;
+	unsigned int id,id1,i,iprod,ipt,Nspectra=20;
 	long long unsigned int NK=0,Np=0,Npi=0;
 	vector<double> spectra_pi,spectra_K,spectra_p;
 	vector<double> error_pi,error_K,error_p;
-	spectra_pi.resize(Nspectrapi);
-	spectra_K.resize(NspectraK);
-	spectra_p.resize(Nspectrap);
-	error_pi.resize(Nspectrapi);
-	error_K.resize(NspectraK);
-	error_p.resize(Nspectrap);
-	for(ipt=0;ipt<Nspectrap;ipt++){
-		spectra_p[ipt]=0.0;
-		error_p[ipt]=0.0;
-	}
-	for(ipt=0;ipt<Nspectrapi;ipt++){
-		spectra_pi[ipt]=0.0;
-		error_pi[ipt]=0.0;
-	}
-	for(ipt=0;ipt<NspectraK;ipt++){
-		spectra_K[ipt]=0.0;
-	    error_K[ipt]=0.0;
+	spectra_pi.resize(Nspectra);
+	spectra_K.resize(Nspectra);
+	spectra_p.resize(Nspectra);
+	error_pi.resize(Nspectra);
+	error_K.resize(Nspectra);
+	error_p.resize(Nspectra);
+	for(ipt=0;ipt<Nspectra;ipt++){
+		spectra_pi[ipt]=spectra_K[ipt]=spectra_p[ipt]=0.0;
+		error_pi[ipt]=error_K[ipt]=error_p[ipt]=0.0;
 	}
 	CHBTPart *part;
 	long long int imc,pid;
@@ -179,19 +152,19 @@ int main(int argc,char *argv[]){
 					ipt=GetIPt_PHENIX(pt);
 					if(ipt>=0){
 						if(pid==211){
-							if(ipt<Nspectrapi)
+							if(ipt<Nspectra)
 								spectra_pi[ipt]+=1;
 							ptbar_pi+=pt;
 							Npi+=1;
 						}
 						if(pid==321){
-							if(ipt<NspectraK)
+							if(ipt<Nspectra)
 								spectra_K[ipt]+=1;	
 							ptbar_K+=pt;
 							NK+=1;
 						}	
 						if(pid==2212){
-							if(ipt<Nspectrap)
+							if(ipt<Nspectra)
 								spectra_p[ipt]+=1;
 							ptbar_p+=pt;
 							Np+=1;
@@ -205,21 +178,16 @@ int main(int argc,char *argv[]){
 		//if((imc+1)%(NMC/10)==0)
 		//	printf("finished %ld percent\n",lrint(100.0*imc/double(NMC)));
 	}
-	for(ipt=0;ipt<Nspectrapi;ipt++){
-		error_pi[ipt]=sqrt(spectra_pi[ipt]);		
-		error_pi[ipt]=1.0E6*(A1/double(NMC))*error_pi[ipt]/GetPTDPTDY_PHENIX(ipt);		
-		spectra_pi[ipt]=1.0E6*(A1/double(NMC))*spectra_pi[ipt]/GetPTDPTDY_PHENIX(ipt);
-	}
-	for(ipt=0;ipt<Nspectrap;ipt++){
-		error_p[ipt]=sqrt(spectra_p[ipt]);
-		error_p[ipt]=1.0E6*(A3/double(NMC))*error_p[ipt]/GetPTDPTDY_PHENIX(ipt);
-		spectra_p[ipt]=1.0E6*(A3/double(NMC))*spectra_p[ipt]/GetPTDPTDY_PHENIX(ipt);
-		
-	}
-	for(ipt=0;ipt<NspectraK;ipt++){
+	for(ipt=0;ipt<20;ipt++){
+		error_pi[ipt]=sqrt(spectra_pi[ipt]);
 		error_K[ipt]=sqrt(spectra_K[ipt]);
-		error_K[ipt]=1.0E6*(A2/double(NMC))*error_K[ipt]/GetPTDPTDY_PHENIX(ipt);
-		spectra_K[ipt]=1.0E6*(A2/double(NMC))*spectra_K[ipt]/GetPTDPTDY_PHENIX(ipt);
+		error_p[ipt]=sqrt(spectra_p[ipt]);
+		error_pi[ipt]=1.0E6*(A/double(NMC))*error_pi[ipt]/GetPTDPTDY_PHENIX(ipt);
+		error_K[ipt]=1.0E6*(A/double(NMC))*error_K[ipt]/GetPTDPTDY_PHENIX(ipt);
+		error_p[ipt]=1.0E6*(A/double(NMC))*error_p[ipt]/GetPTDPTDY_PHENIX(ipt);
+		spectra_pi[ipt]=1.0E6*(A/double(NMC))*spectra_pi[ipt]/GetPTDPTDY_PHENIX(ipt);
+		spectra_K[ipt]=1.0E6*(A/double(NMC))*spectra_K[ipt]/GetPTDPTDY_PHENIX(ipt);
+		spectra_p[ipt]=1.0E6*(A/double(NMC))*spectra_p[ipt]/GetPTDPTDY_PHENIX(ipt);
 	}
 	ptbar_pi=ptbar_pi/double(Npi);
 	ptbar_K=ptbar_K/double(NK);
@@ -227,12 +195,12 @@ int main(int argc,char *argv[]){
 	
 	//printf("Npi=%llu, NK=%llu, Np=%llu\n",Npi,NK,Np);
 	//printf("ptbar_pi=%g, ptbar_K=%g, ptbar_p=%g\n",ptbar_pi,ptbar_K,ptbar_p);
-
-	WriteSpectra(spectra_pi,"spectra/spectra_pi.txt");
-	WriteSpectra(spectra_K,"spectra/spectra_K.txt");
-
+	
+	//WriteSpectra(spectra_pi,"spectra/spectra_pi.txt");
+	//WriteSpectra(spectra_K,"spectra/spectra_K.txt");
 	WriteSpectra(spectra_p,"spectra/spectra_p.txt");
-	double chi2=CalcChiSquared_p(spectra_p, spectra_pi, spectra_K, error_p, error_pi, error_K);
+	printf("howdy doody\n");
+	double chi2=CalcChiSquared_p(spectra_p,error_p);
 	printf("%g\n",chi2);	
 		
 	return 0;
--- run/piper.py
+++ run/piper.py
@@ -7,5 +7,6 @@ y=str(11)
 
 subp=subprocess.Popen(['crap',x,y], stdout=PIPE,stderr=PIPE,universal_newlines=True)
 stdout,stderror = subp.communicate()
-print(stdout)
+#print(isinstance(stdout,str))
+#print(stdout)
 quit()
