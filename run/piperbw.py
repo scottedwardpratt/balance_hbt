0f20dec764fc3c6479cc40d0440fc363b13a298e
Karina Martirosova <karina@pratt-linux.nscl.msu.edu>
2021-10-19 14:47:08 -0400

    update

--- run/piperbw.py
+++ run/piperbw.py
@@ -7,18 +7,24 @@ import math
 import scipy 
 from scipy import optimize
 
-par = np.empty(5, dtype=float)
-par[0]=386
-par[1]=260
-par[2]=170
-par[3]=86
-par[4]=1.2
+par = np.empty(2, dtype=float)
+#par[0]=1000
+#par[1]=800
+#par[2]=750
+par[0]=100
+par[1]=1.13
 
 def chisquare(x0):
-    subp=subprocess.Popen(['bwspectra',str(x0[0]),str(x0[1]),str(x0[2]), str(x0[3]), str(x0[4])], stdout=PIPE,stderr=PIPE,universal_newlines=True)
+    subp=subprocess.Popen(['bwspectra',str(x0[0]),str(x0[1])], stdout=PIPE,stderr=PIPE,universal_newlines=True)
     stdout,stderror = subp.communicate()
+    print(x0[0])
+    print(x0[1])
+#   print(x0[2])
+#    print(x0[3])
+#    print(x0[4])
+    print(stdout)
     return float(stdout)
 
-res = scipy.optimize.minimize(chisquare, par, method='nelder-mead')
+res = scipy.optimize.minimize(chisquare, par, method='powell', options={'disp': True,'return_all': True})
 print(res.x)
 quit()
